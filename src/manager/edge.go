8376d2e4f1d6b567e50b7783c8f0b8364ad62f9b
guoying1030 <gwlii@163.com>
2016-06-15 17:43:43 +0800

    fdsafd

--- src/manager/edge.go
+++ src/manager/edge.go
@@ -3,10 +3,12 @@ package manager
 import "utils"
 
 type Edge struct {
-	Addr string
-	Role string
-	Desc string
-	Loc  *utils.Loc
+	Addr       string
+	Role       string
+	Desc       string
+	Loc        *utils.Loc
+	OutNetWork int64
+	InNetWork  int64
 }
 
 func NewEdge(ip, role, desc string) (e *Edge) {
