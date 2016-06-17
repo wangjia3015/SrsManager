fcfa83f599bb5972492e6b4cbbc8f0dd8d871ba2
guoying1030 <gwlii@163.com>
2016-06-17 11:24:37 +0800

    fdsafd

--- src/manager/srs_server.go
+++ src/manager/srs_server.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/golang/glog"
+	"utils"
 )
 
 const (
@@ -18,11 +19,11 @@ const (
 )
 
 type SrsServer struct {
-	ID         int64
 	Host       string
 	ServerType int
 	Status     int
-
+	Desc       string
+	Loc        utils.Loc
 	Streams           []srs_client.Stream
 	Summary           *srs_client.SummaryData
 	StreamUpdateTime  int64
@@ -37,34 +38,27 @@ func (s *SrsServer) UpdateStatusLoop() {
 	}
 }
 
-func (s *SrsServer) UpdateServerStreams() error {
+func (s *SrsServer) UpdateServerStreams() {
 	if rsp, err := srs_client.GetStreams(s.Host); err != nil {
 		glog.Warningln("UpdateServer GetStreams", s.Host, err)
 		return err
 	} else if rsp.Code != 0 {
 		msg := fmt.Sprintln("GetStream server return err", s.Host, rsp.Code)
 		glog.Warningln(msg)
-		return errors.New(msg)
 	} else {
 		s.Streams = rsp.Streams
 		s.StreamUpdateTime = time.Now().Unix()
-		//glog.Infoln("UpdateServerStreams", s.Streams)
-		return nil
 	}
 }
 
-func (s *SrsServer) UpdateServerSummaries() error {
+func (s *SrsServer) UpdateServerSummaries() {
 	if rsp, err := srs_client.GetSummaries(s.Host); err != nil {
 		glog.Warningln("UpdateServer GetSummaries", s.Host, err)
-		return err
 	} else if rsp.Code != 0 {
 		msg := fmt.Sprintln("GetSummaries server return err", s.Host, rsp.Code)
 		glog.Warningln(msg)
-		return errors.New(msg)
 	} else {
 		s.Summary = &rsp.Data
 		s.SummaryUpdateTime = time.Now().Unix()
-		//glog.Infoln("UpdateServerSummaries", s.Summary)
-		return nil
 	}
 }
