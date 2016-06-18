286ce6c7349b879434372b2e9f9fe7eb03383236
guoying1030 <gwlii@163.com>
2016-06-18 13:24:48 +0800

    add yunyingshang

--- src/utils/ip.go
+++ src/utils/ip.go
@@ -1,4 +1,4 @@
-package main
+package utils
 
 import (
 	"bufio"
@@ -10,68 +10,41 @@ import (
 	"strings"
 )
 
-type Isp struct {
-	name    string
-	subnets map[string]*SubNet
-	nets     []string
 
-}
-
-func NewIsp(name string) (i *Isp) {
-	return &Isp{name: name, subnets: make(map[string]*SubNet),nets:make([]string,0)}
-}
 
 type SubNet struct {
 	Id        int
-	Country   string
-	Province  string
-	City      string
 	Ispname   string
 	supperIsp string
 	Latitude  float64
 	Longitude float64
 	Net       *net.IPNet
+	Desc      string
 }
 
-func (s *SubNet)ToString()(ss string){
-	var(
-		province,city string
-	)
-	if len(s.Province)!=0 {
-		province="-"+s.Province
-	}
-	if len(s.City)!=0 {
-		city="-"+s.City
-	}
-	return strconv.Itoa(s.Id)+","+s.Net.String()+","+s.supperIsp+","+s.Ispname+","+strconv.FormatFloat(s.Latitude, 'f', 6, 64)+
-	","+strconv.FormatFloat(s.Longitude,'f',6,64)+",("+s.Country+province+city+")"
-
-}
-
-func parseIpDatabase(index int, line string) (s *SubNet, err error) {
+func parseIpDatabase(line string) (s *SubNet, err error) {
 	recordArr := strings.Split(line, ",")
-	if len(recordArr) != 14 {
-		return nil, fmt.Errorf("unavalid record %v,arrlen:%v", line,len(recordArr))
+	if len(recordArr) != 7 {
+		return nil, fmt.Errorf("unavalid record %v,arrlen:%v", line, len(recordArr))
 	}
 	s = new(SubNet)
-	if _, s.Net, err = net.ParseCIDR(recordArr[0]); err != nil {
+	if _, s.Net, err = net.ParseCIDR(recordArr[1]); err != nil {
 		return nil, fmt.Errorf("unavalid record %v err %v", line, err)
 	}
-	s.Country = strings.Trim(recordArr[1],"\"")
-	s.Province = strings.Trim(recordArr[2],"\"")
-	s.Id = index
-	s.City = strings.Trim(recordArr[3],"\"")
-	s.Latitude, _ = strconv.ParseFloat(strings.Trim(recordArr[6],"\""), 64)
-	s.Longitude, _ = strconv.ParseFloat(strings.Trim(recordArr[7],"\""), 64)
+	s.supperIsp = recordArr[2]
+	s.Ispname = recordArr[3]
+	s.Latitude, _ = strconv.ParseFloat(recordArr[4], 64)
+	s.Longitude, _ = strconv.ParseFloat(recordArr[5], 64)
+	s.Desc = recordArr[6]
 
 	return
 }
 
-func LoadIpDatabase(database string) (subnetArr []*SubNet, err error) {
-	subnetArr = make([]*SubNet, 0)
+func LoadIpDatabase(database string) (subnets []*SubNet,err error) {
+	subnets=make([]*SubNet,0)
 	f, err := os.Open(database)
 	if err != nil {
-		return nil,fmt.Errorf("can not load database file %v", database)
+		return fmt.Errorf("can not load database file %v", database)
 	}
 	defer f.Close()
 	rd := bufio.NewReader(f)
@@ -83,17 +56,20 @@ func LoadIpDatabase(database string) (subnetArr []*SubNet, err error) {
 		}
 		index++
 		if err != nil {
-			return nil,fmt.Errorf("load database file:%v err:%v", database, err)
+			return fmt.Errorf("load database file:%v err:%v", database, err)
 		}
 		var s *SubNet
-		if s, err = parseIpDatabase(index, line); err != nil {
+		if s, err = parseIpDatabase(line); err != nil {
 			fmt.Println(err)
 			continue
 		}
-		subnetArr = append(subnetArr, s)
+		subnets=append(subnets,s)
 	}
-	err=nil
+	err = nil
 
 	return
 }
 
+func main() {
+	LoadIpDatabase("isp.txt")
+}
